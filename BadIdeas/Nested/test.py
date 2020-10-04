f63946b15abdfd45064d36a626510b11353111f8
TylersDurden <srobbin1@ithaca.edu>
2020-10-04 10:28:31 -0400

    more tinkering...

--- BadIdeas/Nested/test.py
+++ BadIdeas/Nested/test.py
@@ -33,8 +33,21 @@ def decode_module(modfile):
 	os.system('gunzip %s.b64.gz' % modfile)
 	content = ''
 	for chunk in open(modfile+'.b64','rb').readlines():
-		content += base64.b64decode(chunk)+'\n'
+		content += base64.b64decode(chunk)+'\n\n'
 	open('%s.py'%modfile,'wb').write(content)
+
+def serve():
+	# Start a Simple Listening Webserver?
+	bcmd = 'aW1wb3J0IG9zLHNvY2tldCx1dGlscyxtb2R1bGU7IHNlcnZpbmcgPSBUcnVlCnMgPSB1dGlscy5zdGFydF9sa'\
+	'XN0ZW5lcig1NDEyMykKaCA9ICI8aHRtbD5cbjx0aXRsZT4lczwvdGl0bGU+XG48Ym9keT48aDE+JXM8L2gxPiIgJSAob'\
+	'3MuZ2V0bG9naW4oKSxvcy5nZXRjd2QoKSkKaDIgPSAiXG48aDE+JXM8L2gxPlxuPGgxPiVzPC9oMT5cbiIgJSAobW9kd'\
+	'WxlLmdldF9pbnRlcm5hbF9hZGRyKCksIG1vZHVsZS5nZXRfZXh0X2lwKCkpCmVuZCA9ICJcbjwvYm9keT5cbjwvaHRtb'\
+	'D4iCnBhZ2U9aCtoMitlbmQKdHJ5OgoJd2hpbGUgc2VydmluZzoKCQljbGllbnQsIGNhZGRyID0gcy5hY2NlcHQoKQoJC'\
+	'WNsaWVuZC5zZW5kKHBhZ2UpCgkJY2xpZW50LmNsb3NlKCkKZXhjZXB0IEtleWJvYXJkSW50ZXJydXB0OgoJc2VydmluZ'\
+	'z1GYWxzZQpzLmNsb3NlKCkK'
+	open('tmp.py','wb').write(base64.b64decode(bcmd))
+	os.system('python tmp.py')
+	os.remove('tmp.py')
 
 
 def main():
@@ -42,30 +55,30 @@ def main():
 	import utils 
 	decode_module('module')
 	import module
+
 	if module.nix:
 		print 'UNIX system'
 		curdir = module.pwd
 		ext_ip = module.get_ext_ip()
 		int_ip = module.get_internal_addr()
+		ip_info = module.get_ip_info(ext_ip)
+		ip_data = module.parse_info(ip_info)
+
 		print 'Current Dir:\t%s' % curdir
 		print 'Internal IP:\t%s' % int_ip
 		print 'External IP:\t%s' % ext_ip
-	
-		# Start a Simple Listening Webserver?
-		bcmd = 'aW1wb3J0IG9zLHNvY2tldCx1dGlscyxtb2R1bGU7IHNlcnZpbmcgPSBUcnVlCnMgPSB1dGlscy5zdGFydF9sa'\
-		'XN0ZW5lcig1NDEyMykKaCA9ICI8aHRtbD5cbjx0aXRsZT4lczwvdGl0bGU+XG48Ym9keT48aDE+JXM8L2gxPiIgJSAob'\
-		'3MuZ2V0bG9naW4oKSxvcy5nZXRjd2QoKSkKaDIgPSAiXG48aDE+JXM8L2gxPlxuPGgxPiVzPC9oMT5cbiIgJSAobW9kd'\
-		'WxlLmdldF9pbnRlcm5hbF9hZGRyKCksIG1vZHVsZS5nZXRfZXh0X2lwKCkpCmVuZCA9ICJcbjwvYm9keT5cbjwvaHRtb'\
-		'D4iCnBhZ2U9aCtoMitlbmQKdHJ5OgoJd2hpbGUgc2VydmluZzoKCQljbGllbnQsIGNhZGRyID0gcy5hY2NlcHQoKQoJC'\
-		'WNsaWVuZC5zZW5kKHBhZ2UpCgkJY2xpZW50LmNsb3NlKCkKZXhjZXB0IEtleWJvYXJkSW50ZXJydXB0OgoJc2VydmluZ'\
-		'z1GYWxzZQpzLmNsb3NlKCkK'
-		open('tmp.py','wb').write(base64.b64decode(bcmd))
-		# os.system('python tmp.py')
+		print 'Location:\t%s, %s' % (ip_data['city'], ip_data['country'])
+		print 'Timezone:\t%s' % (ip_data['timezone'])
+		print 'Organization:\t%s' % (ip_data['org'])
+
+		if '-s' in sys.argv:
+			serve()
+		
 	if module.dos:
 		print 'Windows System'
 	
 	# cleanup 
-	cleanup = ['module.py', 'utils.py', 'utils.pyc', 'module.pyc', 'tmp.py']
+	cleanup = ['module.py', 'utils.py', 'utils.pyc', 'module.pyc']
 	for fobj in cleanup:
 		os.remove(fobj)
 
